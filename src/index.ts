25fa5670762cc6460c263a0cacb0779a27c26b8d
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-01-31 19:16:17 +0100

    add static fileserver

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import express from "express";
 const PORT = 8080;
 const app = express();
 
-app.listen(PORT);
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
