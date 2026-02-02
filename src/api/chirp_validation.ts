4e7aa692de18bfef72867140c850b3b4d8b11022
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-02-02 17:32:28 +0100

    add censor

--- src/api/chirp_validation.ts
+++ src/api/chirp_validation.ts
@@ -10,19 +10,29 @@ function isChirp(obj: any): obj is Chirp {
 
 export async function handlerChirpValidation(req: Request, res: Response) {
   const chirp = req.body;
-  const maxChirpLength = 140;
-  try {
 
+  const maxChirpLength = 140;
+  const slangs = ["kerfuffle", "sharbert", "fornax"];
+
+
+  try {
     if (!isChirp(chirp)) {
       throw new Error("Something went wrong");
     }
-
     if (chirp.body.length > maxChirpLength) {
       throw new Error("Chirp is too long");
     }
 
+    const chirpWords: string[] = chirp.body.split(" ");
+
+    for (let i = 0; i < chirpWords.length; i++) {
+      if (slangs.includes(chirpWords[i].toLowerCase())) {
+        chirpWords[i] = "****";
+      }
+    }
+
     res.status(200).send({
-      valid: true,
+      cleanedBody: chirpWords.join(" "),
     });
 
   } catch (error) {
