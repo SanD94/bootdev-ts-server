371d2e2b0b33fb7119873cda9153a1ec220e040e
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-02-03 07:19:44 +0100

    add env connection

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
+process.loadEnvFile();
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: requireKey("DB_URL"),
 };
+
+
+function requireKey(key: string): string {
+  const val = process.env[key];
+  if (val === undefined) {
+    throw new Error(`Missing required env variable: ${key}`);
+  }
+  return val;
+}
