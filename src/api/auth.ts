0327dbe2f791e6678604908f09cbe3426f007a71
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-02-12 06:09:52 +0100

    update user login to include refresh token

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,8 +2,11 @@ import { Response, Request } from "express";
 import { getUser } from "../db/queries/users.js";
 import { BadRequestError, UserNotAuthenticatedError } from "./errors.js";
 import { isUser } from "./users.js";
-import { checkPasswordHash, makeJWT } from "../auth.js";
+import { checkPasswordHash, makeJWT, makeRefreshToken } from "../auth.js";
 import { config } from "../config.js";
+import { createRefreshToken } from "../db/queries/refresh_tokens.js";
+import { NewRefreshToken } from "../db/schema.js";
+
 
 export async function handlerLogin(req: Request, res: Response) {
   const user = req.body;
@@ -13,11 +16,6 @@ export async function handlerLogin(req: Request, res: Response) {
     throw new BadRequestError("Something went wrong");
   }
 
-  let duration = user?.expiresInSecond;
-
-  if (!duration || duration > config.jwt.defaultDuration) {
-    duration = config.jwt.defaultDuration;
-  }
 
   const currentUser = await getUser(user.email);
 
@@ -33,11 +31,20 @@ export async function handlerLogin(req: Request, res: Response) {
     throw new UserNotAuthenticatedError("401 Unauthorized");
   }
 
-  const token = makeJWT(signedUser.id, duration, config.jwt.secret);
+  const token = makeJWT(signedUser.id, config.jwt.defaultDuration, config.jwt.secret);
+
+  const expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 60);
+  const refreshToken = await createRefreshToken({
+    token: makeRefreshToken(),
+    userId: signedUser.id,
+    expiresAt: expiryDate,
+  } satisfies NewRefreshToken);
 
   res.status(200).send({
     ...signedUser,
     token,
+    refreshToken: refreshToken.token,
   });
 
 };
