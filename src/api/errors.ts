8730f90a1326b432c3a61a3a32e0a0518731494c
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-02-15 08:10:41 +0100

    remove redundant constructor as custom errors follow the standard one

--- src/api/errors.ts
+++ src/api/errors.ts
@@ -1,35 +1,15 @@
 
 
 // 400
-export class BadRequestError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class BadRequestError extends Error { }
 
 // 401
-export class UserNotAuthenticatedError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class UserNotAuthenticatedError extends Error { }
 
 // 403
-export class UserForbiddenError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class UserForbiddenError extends Error { }
 
 // 404
-export class NotFoundError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class NotFoundError extends Error { }
 
-export class ChirpTooLongError extends BadRequestError {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class ChirpTooLongError extends BadRequestError { }
